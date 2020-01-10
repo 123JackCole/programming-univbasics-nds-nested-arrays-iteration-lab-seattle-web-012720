3a62d8aa5ea3b9f3f34f1667638a4bc1f84fc331
Jack Cole <123jack.cole@gmail.com>
2020-01-10 23:27:49 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -24,7 +24,7 @@ def find_greater_pair(src)
   
   new_array = []
   row_index = 0
-  #larger_num = nil
+  larger_num = nil
   while row_index < src.count do
     element_index = 0
     while element_index < src[row_index].count do
